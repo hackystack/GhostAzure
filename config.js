9ee129aeb8e1e576079b71b8084cd995dad689be
hackystack <dorpalenbarry@gmail.com>
2014-06-17 20:18:03 -0700

    "Completed configuration with Azure"

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'liamblog.azurewebsites.net',
+        url: 'liamsblog.azurewebsites.net',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
